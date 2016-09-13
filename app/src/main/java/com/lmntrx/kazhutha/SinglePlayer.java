0f7846525c5228a917e980d9a72a3e400a8be07e
Nihal Ismail <nihalismailk@gmail.com>
2016-09-13 23:09:36 +0530

    Card generator completed

--- app/src/main/java/com/lmntrx/kazhutha/SinglePlayer.java
+++ app/src/main/java/com/lmntrx/kazhutha/SinglePlayer.java
@@ -1,9 +1,15 @@
 package com.lmntrx.kazhutha;
 
+import android.graphics.Color;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -21,15 +27,17 @@ public class SinglePlayer extends AppCompatActivity {
         for (int i = 0; i < 8; i++){
             String generatedCard=Cards.cardDictonary.get(String.valueOf(rg.nextInt(55)));
             Cards myCard=Cards.getCard(generatedCard);
-            Log.i("Mava",myCard.type);
+            userCards.add(myCard);
+            Log.i("userCards",generatedCard);
         }
 
-        Log.i("1ball",Cards.cardDictonary.get(String.valueOf(rg.nextInt(55))));
+        Cards computerCard=Cards.getCard(Cards.cardDictonary.get(String.valueOf(rg.nextInt(55))));
+        Log.i("computerCard",Cards.cardDictonary.get(String.valueOf(rg.nextInt(55))));
     }
 
     public void generateNewCard(View view)
     {
         Random rg=new Random();
-        Log.i("1ball",Cards.cardDictonary.get(String.valueOf(rg.nextInt(55))));
+        Log.i("nextComputerCard",Cards.cardDictonary.get(String.valueOf(rg.nextInt(55))));
     }
 }
