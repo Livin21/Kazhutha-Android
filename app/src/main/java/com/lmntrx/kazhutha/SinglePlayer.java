793c03817ce7ec12e24b85f08f4c0f714795755d
Aswin CJ <aswincj4@gmail.com>
2016-09-15 23:48:25 +0530

    Design Changes #2
    Things to Complete : Make game Scalable to all devices.

--- app/src/main/java/com/lmntrx/kazhutha/SinglePlayer.java
+++ app/src/main/java/com/lmntrx/kazhutha/SinglePlayer.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -34,6 +36,11 @@ public class SinglePlayer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_single_player);
         //Issue cards once activity is launched
         startGame();
--- app/src/main/java/com/lmntrx/kazhutha/Splash.java
+++ app/src/main/java/com/lmntrx/kazhutha/Splash.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,6 +16,9 @@ public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         TimerTask switchActivity = new TimerTask() {
