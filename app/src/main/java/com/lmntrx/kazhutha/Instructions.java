b8a4c3df2bd3854d6cbc87b79eefc2cba7be509c
Aswin CJ <aswincj4@gmail.com>
2016-09-15 23:07:18 +0530

    Design Changes #1

--- app/src/main/java/com/lmntrx/kazhutha/Instructions.java
+++ app/src/main/java/com/lmntrx/kazhutha/Instructions.java
@@ -13,8 +13,10 @@ public class Instructions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
     }
 
 }
